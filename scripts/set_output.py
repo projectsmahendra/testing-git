38f0685aa540e69ac5d10f26d7adcf532c571e1b
projectsmahendra <mahendra.jecrc.cs@gmail.com>
2024-08-06 18:46:29 +0530

    Update set_output.py

--- scripts/set_output.py
+++ scripts/set_output.py
@@ -5,13 +5,13 @@ def main():
     # Define multiple output variables
     octopus_project_id = "Hello"
 
-    # env_file = os.getenv('GITHUB_ENV')
-    # print(env_file,'-----')
-    # with open(env_file, "a") as outputfile:
-    #     print(f"OCTOPUS_PID={octopus_project_id}")
-    #     outputfile.write(f"OCTOPUS_PID={octopus_project_id}")
-    with open('octopus_project_id.txt', 'w') as f:
-        f.write(octopus_project_id)
+    env_file = os.getenv('GITHUB_ENV')
+    print(env_file,'-----')
+    with open(env_file, "a") as outputfile:
+        print(f"OCTOPUS_PID={octopus_project_id}")
+        outputfile.write(f"OCTOPUS_PID={octopus_project_id}")
+    # with open('octopus_project_id.txt', 'w') as f:
+    #     f.write(octopus_project_id)
 
 if __name__ == "__main__":
     main()
