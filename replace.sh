d7e4ebfa8195c3d039ed8dcccc3ba6d7a372b847
projectsmahendra <mahendra.jecrc.cs@gmail.com>
2024-11-18 07:05:17 +0530

    asdf

--- replace.sh
+++ replace.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sed -i 's/multi_region = false/multi_region = true/' main.tf
-sed -i '/primary_bucket_region = "REPLACE_PRIMARY_REGION_VALUE"/a\  secondary_bucket_region = "REPLACE_SECONDARY_REGION_VALUE"' main.tf
+# sed -i 's/multi_region = false/multi_region = true/' main.tf
+# sed -i '/primary_bucket_region = "REPLACE_PRIMARY_REGION_VALUE"/a\  secondary_bucket_region = "REPLACE_SECONDARY_REGION_VALUE"' main.tf
 
-echo "========"
-cat main.tf
+# echo "========"
+# cat main.tf
+
+
+sed -i 's/app_dir: src/app_dir: mahendra\/src/' workflows/replace-cicd.yml
